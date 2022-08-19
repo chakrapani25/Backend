78287f393b262277f1e343f167d4a7c0150db2d4
unaveengoud <unaveengoud.006@gmail.com>
2022-08-19 21:08:59 +0530

    entered some random text

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -30,7 +30,19 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "Hi this is Uppala Naveen" ;
+        public String ping() {  return "Features\r\n"
+        		+ "Add support for Kubernetes 1.23 (#5519)\r\n"
+        		+ "Improvements\r\n"
+        		+ "Remove support for 1.19 (#5536)\r\n"
+        		+ "Remove support for WindowsServer20H2CoreContainer and WindowsServer2004CoreContainer (#5610)\r\n"
+        		+ "Update docs about ASG tags (#5574)\r\n"
+        		+ "Changed albIngress to awsLoadBalancerController in usage iam-policies example (#5566)\r\n"
+        		+ "Fix typo in error message for IAM service account (#5555)\r\n"
+        		+ "Bug Fixes\r\n"
+        		+ "Fix coredns wait when there are no nodes (#5571)\r\n"
+        		+ "Acknowledgments\r\n"
+        		+ "Weaveworks would like to sincerely thank:\r\n"
+        		+ "@neha-viswanathan, @akhal3d96, @jinnuae40 and @matti" ;
         			
         }
 
