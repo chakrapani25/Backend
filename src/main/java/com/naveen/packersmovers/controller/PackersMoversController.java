241d53450e217fddc03a9b0676e445be39abd819
unaveengoud <unaveengoud.006@gmail.com>
2022-09-03 20:22:23 +0530

    message

--- src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
+++ src/main/java/com/naveen/packersmovers/controller/PackersMoversController.java
@@ -30,7 +30,7 @@ public class PackersMoversController {
         
         
         @GetMapping("/ping")
-        public String ping() {  return "UPPALA NAVEEN GOUD, LAHARI, RADHIKA, SAKETH, VISHNU" ;
+        public String ping() {  return "UPPALA NAVEEN GOUD" ;
         			
         }
 
